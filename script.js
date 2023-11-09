2f015e1858cecee1cbb0496c6871e99d3fdd9d8b
oborysiuk <olga.borysiuk2012@gmail.com>
2023-11-09 19:27:26 +0200

    HW5 - Задача на обробники подій, роботу зі сховищами та атрибутами вмістом

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+
+let button = document.querySelector('.redBtn');
+let body = document.querySelector('.background');
+let container = document.querySelector('.vertical-center');
+let timeInfo = document.querySelector('.time-info');
+let lastUpdate = document.querySelector('.last-update');
+
+button.addEventListener('click', (event)=>{
+    pageChange([button.textContent, Date.now()]);
+})
+
+document.addEventListener('DOMContentLoaded', renderState);
+
+function pageChange(state){
+
+    lastUpdate.innerHTML = '';
+    
+    if (state[0] === 'Turn off'){
+        button.innerHTML = 'Turn on';
+        body.style.backgroundColor = "black";
+        lastUpdate.appendChild(document.createTextNode('Last turn off:'));
+    }
+    else {
+        button.innerHTML = 'Turn off'
+        body.style.backgroundColor = "white";
+        lastUpdate.appendChild(document.createTextNode('Last turn on:'));
+    }
+
+    setTime(new Date(state[1]));
+    storeStateInLocalStorage(button.textContent, timeInfo.textContent);
+}
+
+function pageSet(state){
+
+    if (state[0] === 'Turn on'){
+        button.innerHTML = 'Turn on';
+        body.style.backgroundColor = "black";
+        lastUpdate.appendChild(document.createTextNode('Last turn off:'));
+    }
+    else {
+        button.innerHTML = 'Turn off'
+        body.style.backgroundColor = "white";
+        lastUpdate.appendChild(document.createTextNode('Last turn on:'));
+    }
+
+    setTime(new Date(state[1]));
+}
+
+function setTime(date){
+
+    timeInfo.innerHTML = '';
+    timeInfo.appendChild(document.createTextNode(formatDate(date)));
+
+}
+
+function formatDate(date) {
+    return (
+      [
+        padTwoDigits(date.getDate()),
+        padTwoDigits(date.getMonth() + 1),
+        date.getFullYear()
+      ].join("-") +
+      " " +
+      [
+        padTwoDigits(date.getHours()),
+        padTwoDigits(date.getMinutes()),
+        padTwoDigits(date.getSeconds()),
+      ].join(":")
+    );
+  }
+
+  function padTwoDigits(num) {
+    return num.toString().padStart(2, "0");
+  }
+
+
+  function storeStateInLocalStorage(btnState, lastTimeChange){
+    
+    const state = [btnState,lastTimeChange];
+    localStorage.setItem(['btnState', 'lastTimeChange'], JSON.stringify(state));
+  }
+
+  function renderState() {
+    
+    const state = localStorage.getItem(['btnState', 'lastTimeChange']) !== null 
+    ? JSON.parse(localStorage.getItem(['btnState', 'lastTimeChange'])) 
+    : [];
+
+    pageSet(state);
+}
